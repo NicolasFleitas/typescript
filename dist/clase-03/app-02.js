00efb3b40aa6efb79734284c6c296da18086d31a
oymdev04 <oymdev4@oym.com.py>
2019-11-01 14:09:42 -0300

    aserciones de tipos de datos, alias

---
+++ dist/clase-03/app-02.js
@@ -0,0 +1,18 @@
+"use strict";
+// ASERCIONES DE TIPOS
+// ALIAS PARA TIPOS DE DATOS II
+Object.defineProperty(exports, "__esModule", { value: true });
+var curso = 'TypeScript';
+var cursoNuevo; // esta variable sorpota dos tipos de datos
+cursoNuevo = 'Javascript';
+cursoNuevo = 2; // TypeScript
+// se puede aplicar al tipo de retorno de una funcion
+function getCurso() {
+    //return 'Javascript';
+    return 1;
+}
+// Arreglos
+var cursos = ['Javascript', 'TypeScript'];
+var cursos2 = ['Javascript', 'TypeScript', '1'];
+console.log('cursos', cursos);
+console.log('cursos2', cursos2);
--- src/clase-03/app-02.ts
+++ src/clase-03/app-02.ts
@@ -1,4 +1,5 @@
-// ALIAS PARA TIPOS DE DATOS EN TS
+// ASERCIONES DE TIPOS
+// ALIAS PARA TIPOS DE DATOS II
 
 export {}; // Modulo en TS
 
@@ -7,22 +8,22 @@ let curso: string = 'TypeScript';
 // Cursos código: JavaScript = 1 , TypeScript = 2 
 //curso = 1; no se puede asignar un number en un string para eso vamos a usar alias ( tipo mixto)
 
-let cursoNuevo: string | number; // esta variable sorpota dos tipos de datos
+type Curso = string | number; // alias Curso
+
+let cursoNuevo: Curso // esta variable sorpota dos tipos de datos
 
 cursoNuevo = 'Javascript';
 cursoNuevo = 2; // TypeScript
 
 // se puede aplicar al tipo de retorno de una funcion
 
-function getCurso(): string | number {
+function getCurso(): Curso {
     //return 'Javascript';
     return 1;
 }
-
 // Arreglos
-
 let cursos: string[] = ['Javascript', 'TypeScript'];
-let cursos2: (string | number)[] = ['Javascript', 'TypeScript','1'];
+let cursos2: Curso[] = ['Javascript', 'TypeScript','1'];
 
 console.log('cursos', cursos);
 console.log('cursos2', cursos2);
