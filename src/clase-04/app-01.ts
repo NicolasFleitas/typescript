a7f97e9207826c979f710d5ba61aca838ed43d26
oymdev04 <oymdev4@oym.com.py>
2019-11-01 16:32:00 -0300

    Modificadores de acceso

--- src/clase-04/app-01.ts
+++ src/clase-04/app-01.ts
@@ -2,24 +2,24 @@ export {};
 
 class EscuelaDigital {
     nombre: string;
-    cursos: string[] = [];
+    //cursos: string[] = [];
 
     constructor(nombre: string){
         this.nombre = nombre;
     }
 
-    agregarCurso(curso: string) {
-        this.cursos.push(curso);
-    }
+    // agregarCurso(curso: string) {
+    //     this.cursos.push(curso);
+    // }
 }
 
 const escuela = new EscuelaDigital('EDteam1');
 
-escuela.agregarCurso('Javascript');
-escuela.agregarCurso('TypeScript');
+// escuela.agregarCurso('Javascript');
+// escuela.agregarCurso('TypeScript');
 
 console.log('escuela', escuela);
 console.log('escuela.nombre', escuela.nombre);
-console.log('escuela.curso', escuela.cursos);
+//console.log('escuela.curso', escuela.cursos);
 
 
---
+++ src/clase-04/app-02.ts
@@ -0,0 +1,44 @@
+export {};
+// MIEMBROS PUBLICOS
+
+// EN cualquier ambito donde se encuentre el objeto curso
+// Se podra cambiar su estado
+class Curso {
+    id: number; // public
+    public nombre: string; // public
+
+    constructor(id: number, nombre: string){
+        this.id = id;
+        this.nombre = nombre;
+    }
+}
+
+class EscuelaDigital {
+    nombre: string;
+    cursos: Curso[] = [];
+
+    constructor(nombre: string){
+        this.nombre = nombre;
+    }
+
+    agregarCurso(curso: Curso) {
+        this.cursos.push(curso);
+    }
+}
+
+const escuela = new EscuelaDigital('EDteam1');
+let cursoJS = new Curso(1,'JavaScript');
+let cursoTS = new Curso(2, 'TypeScript');
+
+
+escuela.agregarCurso(cursoJS);
+escuela.agregarCurso(cursoTS);
+
+// Acceder a los atributos publicos
+cursoJS.nombre = 'JavaScript desde Cero';
+cursoJS.id = 20;
+console.log('escuela', escuela);
+console.log('escuela.nombre', escuela.nombre);
+//console.log('escuela.curso', escuela.cursos);
+
+
