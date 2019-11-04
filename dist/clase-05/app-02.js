176f13d713cfb1d5a0e938f87c9feea2cc3a1f5b
oymdev04 <oymdev4@oym.com.py>
2019-11-04 16:58:20 -0300

    avances2

--- dist/clase-05/app-02.js
+++ dist/clase-05/app-02.js
@@ -1,2 +1,11 @@
 "use strict";
 // usaremos una libreria externa JAVASCRIPT: lodash
+// package.json
+// npm init -y 
+// npm install lodash --save
+Object.defineProperty(exports, "__esModule", { value: true });
+var lodash_1 = require("../../node_modules/@types/lodash");
+function numeroAleatorio(a, b) {
+    return lodash_1.random(a, b);
+}
+console.log('numeroAleatorio(1,10)', numeroAleatorio(1, 10));
--- src/clase-05/app-02.ts
+++ src/clase-05/app-02.ts
@@ -1,2 +1,12 @@
 // usaremos una libreria externa JAVASCRIPT: lodash
+// package.json
+// npm init -y 
+// npm install lodash --save
 
+import {random} from '../../node_modules/@types/lodash';
+
+function numeroAleatorio(a: number, b: number) {
+    return random(a, b);
+}
+
+console.log('numeroAleatorio(1,10)',numeroAleatorio(1,10));
